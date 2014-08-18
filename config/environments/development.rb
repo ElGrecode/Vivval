b3353e8cfdecc4116e3aebe47b811cdd1a601258
narwhaldo <narwhaldo@gmail.com>
2014-08-18 04:16:08 -0700

    Changes port for mail usage on Heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              "smtp.gmail.com",
-    port:                 587,
+    port:                 25, #587, locally
     domain:               "vivval.com",
     authenication:        "plain",
     enable_starttls_auto: true,
